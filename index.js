8df6a423f7e7ef0133c91517368485b9db66dcd6
callapa1 <54779545+callapa1@users.noreply.github.com>
2020-11-22 18:31:03 +0100

    Sketchpad finished.

---
+++ index.js
@@ -0,0 +1,57 @@
+const container = document.querySelector('#container');
+const button = document.querySelector('#clearGrid');
+var xAxis = ['0','1','2','3','4','5','6','7','8',
+             '9','10','11','12','13','14','15','16'];
+
+var defaultGridSize = xAxis.length-1;
+var ratio;
+
+createGrid(defaultGridSize);
+
+function createGrid(gridSize){
+  for(let vertical = 0 ; vertical < gridSize ; vertical++){
+    for(let horizontal = 0 ; horizontal < gridSize ; horizontal++){
+      let div = document.createElement('div');
+      div.setAttribute('id',`${xAxis[vertical]}_${horizontal}`);
+      div.classList.add('whiteGrid');
+      container.appendChild(div);
+    }
+  }
+  ratio = 100/gridSize;
+  let divs = container.querySelectorAll('div');
+  let divsLength = divs.length;
+  for(let i = 0 ; i < divsLength ; i++){
+    divs[i].setAttribute('style',`flex: 1 1 ${ratio}%`);
+  }
+}
+
+container.addEventListener('mouseover', (e) => {
+  if(e.target.getAttribute('class') == 'whiteGrid'){
+    e.target.setAttribute('class','blackGrid');
+  }
+})
+
+button.addEventListener('click', function(){removeOldAndCreateNew()});
+
+function removeOldAndCreateNew(){
+  let blackGrid = document.getElementsByTagName('div');
+  let x;
+  let finish = blackGrid.length;
+  for(x = 0 ; x < finish ; x++){
+    blackGrid[x].className = blackGrid[x].
+    className.replace('blackGrid', 'whiteGrid');
+  }
+
+  var newGridSize = Number(prompt("How many squares per side do you "+
+                                  "want in your new grid? (1-100)") || '');
+  
+  let divs = container.getElementsByTagName('div');
+
+  let max = divs.length;
+
+  while(container.firstChild){
+
+    container.removeChild(container.firstChild);
+  }
+  createGrid(newGridSize);
+}
